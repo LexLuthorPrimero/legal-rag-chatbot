fac92f14f2c98a717990d0f5a9e24028c51fafd9
Lucas <tu_email@gmail.com>
2026-04-21 17:15:38 -0300

    Corregir dependencias: agregar groq, fastembed y pinecone correctamente

--- src/utils/config.py
+++ src/utils/config.py
@@ -3,23 +3,14 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-def get_openai_api_key():
-    return os.getenv("OPENAI_API_KEY")
-
 def get_pinecone_api_key():
     return os.getenv("PINECONE_API_KEY")
 
 def get_pinecone_environment():
-    return os.getenv("PINECONE_ENVIRONMENT")
+    return os.getenv("PINECONE_ENVIRONMENT", "gcp-starter")
 
 def get_pinecone_index_name():
-    return os.getenv("PINECONE_INDEX_NAME", "legal-chatbot")
-
-def get_s3_bucket():
-    return os.getenv("S3_BUCKET_NAME")
+    return os.getenv("PINECONE_INDEX_NAME", "legal-rag")
 
 def get_groq_api_key():
     return os.getenv("GROQ_API_KEY")
-def get_groq_api_key():
-    return os.getenv("GROQ_API_KEY")
-
